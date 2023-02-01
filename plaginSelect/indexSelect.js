379ddc4db7dd69acd02153ec5ba7abea266d34d2
Pozharskiy91 <skay-deman@mail.ru>
2023-02-01 21:35:10 +0300

    select готов

--- plaginSelect/indexSelect.js
+++ plaginSelect/indexSelect.js
@@ -2,7 +2,19 @@ import {Select} from './select/select'
 import './select/style.scss'
 
 const select = new Select('#select', {
-
+    placeholder: 'Выбери пожалуйто элемент',
+    selectedId: '4',
+    data: [
+        {id: '1', value:'React'},
+        {id: '2', value:'Angular'},
+        {id: '3', value:'Vue'},
+        {id: '4', value:'React Native'},
+        {id: '5', value:'Next'}
+    ],
+    onSelect(item){
+        console.log('Selected Item', item)
+    }
+    
 })
 
 window.s = select
--- plaginSelect/select/select.js
+++ plaginSelect/select/select.js
@@ -1,17 +1,26 @@
-const getTemplate = () => {
+const getTemplate = (data = [], placeholder, selectedId) => {
+    let text = placeholder ?? 'Placeholder по умолчанию'
+
+    const items = data.map(item =>{
+        let cls = ''
+        if(item.id === selectedId) {
+                text = item.value
+                cls = 'selected'
+            }
+        return`
+        <li class="select__item ${cls}" data-type = "item" data-id="${item.id}">${item.value}</li>
+        `
+})
+//TODO Закончил сдесь 57:19
     return`
-        <div class="select__input">
-            <span>Text</span>
-            <i class="fa fa-list-alt"></i>
+        <div class="select__backdrop" data-type ="backdrop"></div>
+        <div class="select__input" data-type="input">
+            <span data-type="value">${text}</span>
+            <i class="fa fa-arrow-down" data-type="arrow"></i>
         </div>
         <div class="select__dropdown">
             <ul class="select__list">
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
-                <li class="select__item">123</li>
+              ${items.join('')}
             </ul>
         </div>
         `
@@ -20,19 +29,83 @@ const getTemplate = () => {
 export class Select{
     constructor(selector, options){
         this.$el = document.querySelector(selector)
+        this.options = options
+        this.selectedId = options.selectedId
 
         this.#render()
+        this.#setup()
+        
     }
     
+    //Работа с шаблоном
     #render(){
-        this.$el.innerHTML = getTemplate()
+        const{placeholder, data} = this.options
+        this.$el.classList.add('select')
+        this.$el.innerHTML = getTemplate(data, placeholder, this.selectedId)
+    }
+
+    //Работа с настройкой
+    #setup(){
+        this.clickHandler = this.clickHandler.bind(this)
+        this.$el.addEventListener('click', this.clickHandler)
+        this.$arrow = this.$el.querySelector('[data-type="arrow"]')
+        this.$value = this.$el.querySelector('[data-type="value"]')
+    }
+
+    clickHandler(event){ //обработка события клика
+        const {type} = event.target.dataset
+        
+        if (type === 'input'){
+            this.toggle()
+        } else if (type ==='item')
+        {const id = event.target.dataset.id
+        this.select(id)
+        } else if (type === 'backdrop')
+        this.clouse()
+    }
+
+    get isOpen(){
+        return this.$el.classList.contains('open') //Проверка состояния select
+    }
+
+    get current() {
+        return this.options.data.find(item=>item.id === this.selectedId)
+    }
+
+    select (id){
+        this.selectedId = id
+        this.$value.textContent = this.current.value
+
+        this.$el.querySelectorAll('[data-type="item"]').forEach(el => {
+            el.classList.remove('selected')
+        })
+        this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected')
+
+        this.options.onSelect ? this.options.onSelect(this.current):null
+
+        this.clouse()
+    }
+
+    //Закрыть или открыть select в зависимости от его состояния 
+    toggle(){
+        this.isOpen ? this.clouse() : this.open()
     }
 
     open(){
         this.$el.classList.add('open')
+        this.$arrow.classList.remove('fa-arrow-down')
+        this.$arrow.classList.add('fa-arrow-up')
     }
 
     clouse(){
         this.$el.classList.remove('open')
+        this.$arrow.classList.remove('fa-arrow-up')
+        this.$arrow.classList.add('fa-arrow-down')
+        
+    }
+
+    destroy(){
+        this.$el.removeEventListener('click', this.clickHandler)
+        this.$el.innerHTML = ''
     }
 }
